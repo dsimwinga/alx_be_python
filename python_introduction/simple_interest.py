910c0d06df483a729e43ad611d689f0c49529553
DANNY SIMWINGA <infor.dsimwinga@gmail.com>
2024-12-12 11:01:42 +0200

    Simple interest calculated

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,16 @@
+#Declaring Variables
+#PRINCIPAL
+P = 1000
+
+#RATE IN %
+R = 0.05
+
+#TIME IN YEARS
+T = 3
+
+#CALCULATING THE INTEREST
+interest = P * R * T
+
+#Displaying the Interest to the user
+print(f"The simple interest is: {interest}")
+
