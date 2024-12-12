d3d611be0c85562d09f57cbd56d7c53f4254fe81
DANNY SIMWINGA <infor.dsimwinga@gmail.com>
2024-12-12 11:25:25 +0200

    hours to seconds convertion added

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,11 @@
+#Declaring variables
+#Hours
+hours = 2
+
+#Seconds
+seconds =3600
+
+#Doing calculations
+result = hours * seconds
+#Printing to the user
+print(f"{hours} hour(s) is {result} seconds.")
