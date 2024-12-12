ecdfca889879106c9bb40c0dc98423f42dd7fc11
DANNY SIMWINGA <infor.dsimwinga@gmail.com>
2024-12-12 10:35:27 +0200

    BASIC OPERATIONS IN PYTHON

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+number1 = 10
+number2 = 5
+
+#ADDITION OF NUMBER1 AND NUMBER2
+sum = number1 + number2
+
+#SUBTRACTION OF NUMBER1 AND NUMBER2
+difference = number1 - number2
+
+#MULTIPLICATION OF NUMBER1 AND NUMBER2
+product = number1 * number2
+
+
+print(f"Addition of 10 and 5 is {sum}")
+print(f"Subtraction of 10 and 5 is {difference}")
+print(f"Multiplication of 10 and 5 is {product}")
