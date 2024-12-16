57a9ba7eeaa4e7a4231db380fbe7f43228bac329
DANNY SIMWINGA <infor.dsimwinga@gmail.com>
2024-12-16 12:18:08 +0200

    errors eliminated

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,13 +1,13 @@
-weather_today = input("What's the weather like today? (sunny/rainy/cold): ")
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
-if weather_today == 'sunny':
+if weather == 'sunny':
     print("Wear a t-shirt and sunglasses.")
     
     
-elif weather_today == 'rainy':
+elif weather == 'rainy':
     print("Wear a t-shirt and sunglasses.")
     
-elif weather_today == 'cold':
+elif weather == 'cold':
     print("Make sure to wear a warm coat and a scarf.")
     
 else:
