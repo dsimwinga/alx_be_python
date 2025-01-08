ce38aeb5e22309f7d9f946f393cadfbf84831cc3
DANNY SIMWINGA <infor.dsimwinga@gmail.com>
2025-01-08 12:35:43 +0200

    Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,14 +2,13 @@ def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
     
-    elif operation == 'subtract':
+    if operation == 'subtract':
         return num1 - num2
     
-    elif operation == 'multiply':
+    if operation == 'multiply':
         return num1 * num2
     
+    if num2==0:
+        print("Second Number should not be 0")
     else:
-        if num2 == 0 and operation == 'divide':
-            return 0
-        else:
-            return num1 / num2
+        return num1 / num2
