be573832e17a16967d4e53ec35bdae13de742090
DANNY SIMWINGA <infor.dsimwinga@gmail.com>
2025-01-16 09:44:38 +0200

    Unit Tests for a Simple Calculator Class

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,15 +6,15 @@ class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
         """This method will run before each test."""
-        self.calc = SimpleCalculator()  # Create an instance of the calculator (now using 'calc')
+        self.calc = SimpleCalculator()  # Create an instance of the calculator
 
-    def test_add(self):
+    def test_addition(self):
         """Test the add method."""
         self.assertEqual(self.calc.add(2, 3), 5)  # Test addition 2 + 3
         self.assertEqual(self.calc.add(-1, 1), 0)  # Test addition -1 + 1
         self.assertEqual(self.calc.add(-1, -1), -2)  # Test addition -1 + -1
 
-    def test_subtract(self):
+    def test_subtraction(self):
         """Test the subtract method."""
         self.assertEqual(self.calc.subtract(5, 3), 2)  # Test subtraction 5 - 3
         self.assertEqual(self.calc.subtract(3, 5), -2)  # Test subtraction 3 - 5
@@ -26,7 +26,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(0, 5), 0)  # Test multiplication 0 * 5
         self.assertEqual(self.calc.multiply(-2, 3), -6)  # Test multiplication -2 * 3
 
-    def test_divide(self):
+    def test_division(self):
         """Test the divide method."""
         self.assertEqual(self.calc.divide(6, 2), 3)  # Test division 6 / 2
         self.assertEqual(self.calc.divide(5, 0), None)  # Test division by zero, expecting None
